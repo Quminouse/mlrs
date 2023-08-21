d435c140331746f8c6abf655168f9b31729bb351
jackson pegg <jacksonpegg01@outlook.com>
2023-08-20 22:35:26 -0400

    Remove generic matrices and implement forward function

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,52 +1,52 @@
-use rand::{Rng, prelude::Distribution, distributions::Standard};
+use rand::Rng;
 
-pub struct Matrix<T> {
+#[derive(Clone)]
+pub struct Matrix {
     rows: usize,
     cols: usize,
-    data: Vec<T>,
+    pub data: Vec<f32>,
 }
 
-impl<T: Copy> Matrix<T> {
+impl Matrix {
+    pub fn get_mut(&mut self, row: usize, col: usize) -> &mut f32 {
+        assert!(self.data.len() > self.cols * row + col);
+        &mut self.data[self.cols * row + col]
+    }
+
+    pub fn get(&self, row: usize, col: usize) -> &f32 {
+        assert!(self.data.len() > self.cols * row + col);
+        &self.data[self.cols * row + col]
+    }
+
     pub fn alloc(rows: usize, cols: usize) -> Self {
         Matrix {
             rows,
             cols,
-            data: Vec::with_capacity(rows * cols),
+            data: vec![0.0; rows * cols], 
         }
     }
-    pub fn fill(&mut self, value: T) {
+
+    pub fn fill(&mut self, value: f32) {
         self.data.clear();
         for _ in 0..self.data.capacity() {
             self.data.push(value);
         }
     }
-    fn get_mut(&mut self, row: usize, col: usize) -> &mut T {
-        &mut self.data[self.cols * row + col]
-    }
-    fn get(&self, row: usize, col: usize) -> &T {
-        &self.data[self.cols * row + col]
-    }
-}
 
-impl <T: Copy> Matrix<T> where Standard: Distribution<T> {
     pub fn rand(&mut self) {
-        self.data.clear();
         let mut rng = rand::thread_rng();
         for _ in 0..self.data.capacity() {
             self.data.push(rng.gen());
         }
     }
 }
-
 // Multiplication
-impl<T: std::ops::Mul<Output = T> + Copy + std::default::Default + std::ops::AddAssign>
-    std::ops::Mul for Matrix<T>
-{
-    type Output = Matrix<T>;
+impl std::ops::Mul for Matrix {
+    type Output = Matrix;
 
     fn mul(self, rhs: Self) -> Self::Output {
         assert!(self.cols == rhs.rows);
-        let mut dst: Matrix<T> = Matrix::alloc(self.rows, rhs.cols);
+        let mut dst: Matrix = Matrix::alloc(self.rows, rhs.cols);
         dst.fill(Default::default());
         for i in 0..dst.rows {
             for j in 0..dst.cols {
@@ -59,12 +59,10 @@ impl<T: std::ops::Mul<Output = T> + Copy + std::default::Default + std::ops::Add
     }
 }
 
-impl<T: std::ops::Mul<Output = T> + Copy + std::default::Default + std::ops::AddAssign>
-    std::ops::MulAssign for Matrix<T>
-{
+impl std::ops::MulAssign for Matrix {
     fn mul_assign(&mut self, rhs: Self) {
         assert!(self.cols == rhs.rows);
-        let mut dst: Matrix<T> = Matrix::alloc(self.rows, rhs.cols);
+        let mut dst: Matrix = Matrix::alloc(self.rows, rhs.cols);
         dst.fill(Default::default());
         for i in 0..dst.rows {
             for j in 0..dst.cols {
@@ -73,12 +71,13 @@ impl<T: std::ops::Mul<Output = T> + Copy + std::default::Default + std::ops::Add
                 }
             }
         }
+        *self = dst.clone();
     }
 }
 
 // Addition
-impl<T: std::ops::Add<Output = T> + Copy> std::ops::Add for Matrix<T> {
-    type Output = Matrix<T>;
+impl std::ops::Add for Matrix {
+    type Output = Matrix;
 
     fn add(mut self, rhs: Self) -> Self::Output {
         assert!(rhs.rows == self.rows);
@@ -91,7 +90,7 @@ impl<T: std::ops::Add<Output = T> + Copy> std::ops::Add for Matrix<T> {
     }
 }
 
-impl<T: std::ops::Add<Output = T> + Copy> std::ops::AddAssign for Matrix<T> {
+impl std::ops::AddAssign for Matrix {
     fn add_assign(&mut self, rhs: Self) {
         assert!(rhs.rows == self.rows);
         assert!(rhs.cols == self.cols);
@@ -103,7 +102,7 @@ impl<T: std::ops::Add<Output = T> + Copy> std::ops::AddAssign for Matrix<T> {
 }
 
 // Debug
-impl<T: std::fmt::Debug + Copy> std::fmt::Debug for Matrix<T> {
+impl std::fmt::Debug for Matrix {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         writeln!(f, "")?;
         write!(f, "[ ")?;
@@ -113,7 +112,23 @@ impl<T: std::fmt::Debug + Copy> std::fmt::Debug for Matrix<T> {
                 write!(f, "]")?;
             } else if (i + 1) % self.cols == 0 {
                 write!(f, "\n  ")?;
-            }         }
+            }
+        }
         Ok(())
+    }
+}
+
+pub trait Sigmoid {
+    fn sigmoid(&mut self);
+}
+
+impl Sigmoid for Matrix {
+    fn sigmoid(&mut self) {
+        for i in 0..self.rows {
+            for j in 0..self.cols {
+                let e = 2.71828f32;
+                *self.get_mut(i, j) = 1.0f32 / (1.0f32 + (e.powf(*self.get(i, j))));
+            }
+        }
     }
 }
